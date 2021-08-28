7a75fc0825dae6ad41fba315710ef05fe16f6cea
AlexNoelHdz <alejandro.noel.hernandez@gmail.com>
2021-08-28 09:11:45 -0500

    Max Peso

--- Class_2021-08-28/class_2021-08-28.py
+++ Class_2021-08-28/class_2021-08-28.py
@@ -9,11 +9,15 @@ import pandas as pd
 df = pd.read_excel('datosPE.xlsx', sheet_name = 'Hoja1')
 
 # - El vector de IMC
-
+df_IMC = df['Peso']/df['Estatura']**2
 # - La media de los pesos y la media de las estaturas
+media_pesos = df['Peso'].mean()
+media_estatura = df['Estatura'].mean()
+
+#Ejemplo indexar
 
 # - El ID de la persona de mayor peso, de la persona de menor estatura
-
+mayor_peso = df[max(df['Peso'])]['ID Persona']
 # - El vector de los ID de las personas con IMC>=28
 
 # Todos los resultados se ponen en una variable y se imprimen.
