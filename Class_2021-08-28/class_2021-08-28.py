36c345b80f32508bb78658397fde259a82942e57
AlexNoelHdz <alejandro.noel.hernandez@gmail.com>
2021-08-28 08:42:23 -0500

    Vector IMC

--- Class_2021-08-28/class_2021-08-28.py
+++ Class_2021-08-28/class_2021-08-28.py
@@ -4,5 +4,16 @@ Created on Sat Aug 28 08:20:10 2021
 
 @author: Alejandro Hernandez
 """
-import pandas as hola
+import pandas as pd
+#Jalar la información de un archivo
+df = pd.read_excel('datosPE.xlsx', sheet_name = 'Hoja1')
 
+# - El vector de IMC
+
+# - La media de los pesos y la media de las estaturas
+
+# - El ID de la persona de mayor peso, de la persona de menor estatura
+
+# - El vector de los ID de las personas con IMC>=28
+
+# Todos los resultados se ponen en una variable y se imprimen.
