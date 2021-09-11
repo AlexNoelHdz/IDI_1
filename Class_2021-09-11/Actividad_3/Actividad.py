617bb1d1a66581a4bc448a3d4cf8e45c09f2cf57
AlexNoelHdz <alejandro.noel.hernandez@gmail.com>
2021-09-11 10:06:09 -0500

    Actividad 3

--- Class_2021-09-11/Actividad_3/Actividad.py
+++ Class_2021-09-11/Actividad_3/Actividad.py
@@ -15,22 +15,26 @@ Genere una lista M de 100 números aleatorios reales en [3,10)
 Imprima la suma de los elementos de M
 Genere una lista R con 20 listas de 50 números reales en [0,10)
 Cada uno de los incisos debe resolverse con una sola línea de código.
+#Contar booleanos
+# pares = L%2==0
 """
+import numpy as np
 # Inicialice la semilla de aleatoriedad con un valor fijo arbitrario
-
+np.random.seed(666)
 # Genere una lista G con los elementos {3,6,9,...,39}
-
+G = np.arange(3,41,3)
 # Imprima una permutación aleatoria de los elementos de G.
-
+np.random.shuffle(G)
+print("{}".format(G))
 # Obtenga una lista H con 4 elementos de G seleccionados de forma aleatoria (sin reemplazo).
-
+H = np.random.choice(G, 4,False)
 # Genere un arreglo L de 100 números aleatorios enteros en [5,100]
-
-# Imprima la cantidad de números pares en L.
-
+L = np.random.randint(5,100,(100))
+# Imprima la cantidad de números pares en L. 
+print(sum(L%2==0))
 # Genere una lista M de 100 números aleatorios reales en [3,10)
-
+M = np.random.uniform(3,10,100)
 # Imprima la suma de los elementos de M
-
+print(sum(M))
 # Genere una lista R con 20 listas de 50 números reales en [0,10)
-
+R = list(list(np.random.uniform(0, 10, 50)) for i in range(20))
