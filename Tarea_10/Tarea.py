12c58eb90517e5e5d1b620631adb27d4b42698ed
Alejandro Hernandez Gutierrez <alejandro.hernandez.gutierrez@intel.com>
2021-11-24 22:42:26 -0600

    Resolviendo caso desbalanceado

--- Tarea_10/Tarea.py
+++ Tarea_10/Tarea.py
@@ -4,28 +4,39 @@
 """
 def tupla_min_max(izq, der, tipo):
        if tipo == True:
-           return (max(izq, der), 'IZQUIERDA' if izq >= der else 'DERECHA')
+           if izq.dato >= der.dato:
+               return izq, 'IZQUIERDA'
+           return der, 'DERECHA'
+       else:
+           if izq.dato <= der.dato:
+               return izq, 'IZQUIERDA'      
+           return der, 'DERECHA' 
            
-       return (min(izq, der), 'IZQUIERDA' if izq <= der else 'DERECHA')
-   
 class nodo:
     def __init__(self,dato):
         self.dato=dato
         self.izq=None
         self.der=None
                 
-    def min_max(self, tipo, prof):
+    def min_max(self, tipo = True, prof = 1):
+            # print(self.dato)
+            #Nada en hijos o profundidad no mayor que 1
+            if (self.izq is None and self.der is None) or not prof > 1:
+                return self, 'RAIZ'
+            
+            #Caso desbalanceado 1
+            if (self.izq is None and self.der is not None):
+                return self.der.min_max(not tipo, prof-1)
+            #Caso desbalanceado 2
+            if (self.izq is not None and self.der is None):
+                return self.izq.min_max(not tipo, prof-1)
+            
+            
+            nodo_izq, _ = self.izq.min_max(not tipo, prof-1)
+            nodo_der, _ = self.der.min_max(not tipo, prof-1)
         
-            if (self.izq is None and self.der is None) or prof == 1:
-                return self.dato, 'RAIZ'
-            
-            if self.izq is not None:
-                value_izq, _ = self.izq.min_max(not tipo, prof-1)
-            if self.der is not None:
-                value_der, _ = self.der.min_max(not tipo, prof-1)
-            
                 
-            return tupla_min_max(value_izq, value_der, tipo)
+            return tupla_min_max(nodo_izq, nodo_der, tipo)
 # %%MiniMax Arbol
 # Level 1
 arbolito = nodo(0)
@@ -54,13 +65,13 @@ arbolito.der.der.izq = nodo(1)
 arbolito.der.der.der = nodo(8)
 
 # %%MinMax Tests
-test = arbolito.min_max(True, 4)
-print(test)
-test = arbolito.min_max(False, 4)
-print(test)
-test = arbolito.min_max(True, 3)
-print(test)
-test = arbolito.min_max(False, 3)
-print(test)
+nodo, lado = arbolito.min_max(True, 4)
+print(nodo.dato, lado)
+nodo, lado =  arbolito.min_max(False, 4)
+print(nodo.dato, lado)
+nodo, lado =  arbolito.min_max(True, 3)
+print(nodo.dato, lado)
+nodo, lado =  arbolito.min_max(False, 3)
+print(nodo.dato, lado)
 
 
